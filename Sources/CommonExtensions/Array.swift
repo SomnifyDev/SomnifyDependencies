b832828abd2feb8fdc84d25bfc9c0f8d7c853b3f
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2022-03-22 22:38:26 +0300

    Added new extension for Array

---
+++ Sources/CommonExtensions/Array.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public extension Array where Element: FloatingPoint {
+
+    func sum() -> Element {
+        return self.reduce(0, +)
+    }
+
+    func meanValue() -> Element {
+        return self.sum() / Element(self.count)
+    }
+
+    func standardDeviation() -> Element {
+        let mean = self.meanValue()
+        let v = self.reduce(0, { $0 + ($1-mean)*($1-mean) })
+        return sqrt(v / (Element(self.count) - 1))
+    }
+
+}
